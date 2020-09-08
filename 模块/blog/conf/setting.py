b3c47a150a56eb2b27b80c585c5efa0acd83895a
SageYan <wow1989914@163.com>
2020-09-08 17:28:43 +0800

    blog homework

---
+++ 模块/blog/conf/setting.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+register_path = os.path.join(BASE_PATH, 'db', 'register')
+print(register_path)
--- 模块/blog/core/src.py
+++ 模块/blog/core/src.py
@@ -1,13 +1,9 @@
-[TOC]
-
-
-
-# Python全栈学习（25）博客园登录模拟作业
-
-### 逻辑脚本
-
-```python
+# inner module
 import hashlib
+
+# custom module
+from conf import setting
+from lib import common
 
 status_dic = {
     'username': None,
@@ -23,21 +19,22 @@ def get_md5(password):
 
 
 def register():
-    with open('register', 'r', encoding='utf-8') as f1:
+    with open(setting.register_path, 'r', encoding='utf-8') as f1:
         dic = {i.strip().split('|')[0]: i.strip().split('|')[1] for i in f1}
+        #print(dic)
     while 1:
         print('\033[1;45m 欢迎来到注册页面 \033[0m')
         username = input("请输入用户名： ").strip()
         if not username.isalnum():
             print('\033[1;31;0m 用户名有非法字符，请重新输入 \033[0m')
             continue
-        if username in dic:
+        if username in dic.keys():
             print('\033[1;31;0m 用户名已经存在，请重新输入 \033[0m')
             continue
         password = input("请输入密码： ").strip()
         if 3 < len(password) <= 14:
             password = get_md5(password)
-            with open('register', 'a', encoding='utf-8') as f2:
+            with open(setting.register_path, 'a', encoding='utf-8') as f2:
                 f2.write('\n{}|{}'.format(username, password))
 
                 status_dic['username'] = str(username)
@@ -50,8 +47,9 @@ def register():
 
 def login():
     i = 0
-    with open('register', encoding='utf-8') as f1:
+    with open(setting.register_path, encoding='utf-8') as f1:
         dic = {i.strip().split('|')[0]: i.strip().split('|')[1] for i in f1}
+        #print(dic)
     while i < 3:
         username = input('请输入用户名：').strip()
         password = get_md5(input('请输入密码：').strip())
@@ -66,36 +64,24 @@ def login():
             i += 1
 
 
-def auth(func):
-    def inner(*args, **kwargs):
-        if status_dic['status']:
-            ret = func(*args, **kwargs)
-            return ret
-        else:
-            print('\033[1;31;0m 请先进行登录 \033[0m')
-            if login():
-                ret = func(*args, **kwargs)
-                return ret
-
-    return inner
 
 
-@auth
+@common.auth
 def article():
     print("\033[1;32;0m 欢迎{}访问文章页面\033[0m".format(status_dic["username"]))
 
 
-@auth
+@common.auth
 def diary():
     print("\033[1;32;0m 欢迎{}访问日记页面\033[0m".format(status_dic["username"]))
 
 
-@auth
+@common.auth
 def comment():
     print("\033[1;32;0m 欢迎{}访问评论页面\033[0m".format(status_dic["username"]))
 
 
-@auth
+@common.auth
 def enshrine():
     print("\033[1;32;0m 欢迎{}访问评论页面\033[0m".format(status_dic["username"]))
 
@@ -123,27 +109,30 @@ choice_dict = {
     8: exit_program,
 }
 
-while flag:
-    print('''
-    欢迎来到博客园首页
-    1:请注册
-    2:请登录
-    3:文章页面
-    4:日记页面
-    5:评论页面
-    6:收藏页面
-    7:注销
-    8:退出程序''')
 
-    choice = input('请输入您选择的序号:').strip()
-    if choice.isdigit():
-        choice = int(choice)
-        if 0 < choice <= len(choice_dict):
-            choice_dict[choice]()
+def run():
+
+
+    while flag:
+        print('''
+        欢迎来到博客园首页
+        1:请注册
+        2:请登录
+        3:文章页面
+        4:日记页面
+        5:评论页面
+        6:收藏页面
+        7:注销
+        8:退出程序''')
+
+        choice = input('请输入您选择的序号:').strip()
+        if choice.isdigit():
+            choice = int(choice)
+            if 0 < choice <= len(choice_dict):
+                choice_dict[choice]()
+            else:
+                print('\033[1;31;0m 您输入的超出范围，请重新输入 \033[0m')
         else:
-            print('\033[1;31;0m 您输入的超出范围，请重新输入 \033[0m')
-    else:
-        print('\033[1;31;0m 您您输入的选项有非法字符，请重新输入 \033[0m')
+            print('\033[1;31;0m 您您输入的选项有非法字符，请重新输入 \033[0m')
 
-```
 
